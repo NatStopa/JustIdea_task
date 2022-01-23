129e5acded35edb2a60291a4f46188c42d11cdc0
NatStopa <nat.stopa.nat@gmail.com>
2022-01-23 12:22:34 +0100

    Made page responsive on small devices

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 document.querySelectorAll(".icon").forEach(function () {
   const icons = document.querySelectorAll(".icon");
+  const image = document.querySelector(".gearwheel");
   const circle = 1;
-  let radius = "300px";
+  let radius = image.clientHeight / 2;
   const start = 0;
   const iconsNr = icons.length;
   let angle = (360 * circle) / iconsNr;
@@ -9,6 +10,6 @@ document.querySelectorAll(".icon").forEach(function () {
     let icon = icons[i];
     let rotate = angle * i + start;
     let rotateReverse = rotate * -1;
-    icon.style.transform = `rotate(${rotate}deg) translate(${radius}) rotate(${rotateReverse}deg) `;
+    icon.style.transform = `rotate(${rotate}deg) translate(${radius}px) rotate(${rotateReverse}deg) `;
   }
 });
